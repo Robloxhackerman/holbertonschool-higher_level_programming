a76ee1b38fd3051f252ee22aa2eba43aaece9ad8
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-09-08 10:33:34 -0700

    try3

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-PEPE = number % 10
+if number < 0:
+    PEPE = ((number * -1) % 10) * -1
+elif number >= 0:
+    PEPE = number % 10
+
 if PEPE == 0:
     print(f"Last digit of {number} is {PEPE} and is 0")
 elif PEPE > 5:
