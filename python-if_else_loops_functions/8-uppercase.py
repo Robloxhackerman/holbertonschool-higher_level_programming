2c02cb726d8ec412e88f9de7f21c17f6660e6e6c
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-09-09 11:24:29 -0700

    python-samaaaaaa

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def uppercase(str):
-   PEPEXTO = ""
-   for PEPE1 in range(len(str)):
-       if ord(str[PEPE1]) > 96 and ord(str[PEPE1]) < 123:
-           PEPEXTO = PEPEXTO + chr(ord(str[PEPE1]) - 32)
-           continue
-       PEPEXTO = PEPEXTO + str[PEPE1]
-   print("{}".format(PEPEXTO))
+    PEPEXTO = ""
+    for PEPE1 in range(len(str)):
+        if ord(str[PEPE1]) > 96 and ord(str[PEPE1]) < 123:
+            PEPEXTO = PEPEXTO + chr(ord(str[PEPE1]) - 32)
+            continue
+        PEPEXTO = PEPEXTO + str[PEPE1]
+    print("{}".format(PEPEXTO))
