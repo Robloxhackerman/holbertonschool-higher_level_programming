93c1da10935dc5f41a8243bf0a1faf466307376e
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-09-09 10:19:45 -0700

    try3

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,10 +2,10 @@
 def fizzbuzz():
     for PEPE1 in range(0, 101):
         if (PEPE1 % 3) == 0 and (PEPE1 % 5) == 0:
-            print(" FizzBuzz")
+            print(" FizzBuzz",end="")
         elif (PEPE1 % 5) == 0:
-            print(" Buzz")
+            print(" Buzz", end="")
         elif (PEPE1 % 3) == 0:
-            print(" Fizz")
+            print(" Fizz",end="")
         else:
-            print("{}".format(PEPE1))
+            print("{}".format(PEPE1),end="")
