b5d89a9062971351a0f4b3d1407d25ce1abe64b3
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-09-09 11:30:29 -0700

    try anda a saber

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -3,11 +3,11 @@ def fizzbuzz():
     print("1 2", end="")
     for PEPE1 in range(3, 101):
         if (PEPE1 % 3) == 0 and (PEPE1 % 5) == 0:
-            print(" FizzBuzz",end="")
+            print(" FizzBuzz", end="")
         elif (PEPE1 % 5) == 0:
             print(" Buzz", end="")
         elif (PEPE1 % 3) == 0:
-            print(" Fizz",end="")
+            print(" Fizz", end="")
         else:
-            print(" {}".format(PEPE1),end="")
-    print(" ")
+            print(" {}".format(PEPE1), end="")
+    print(" ", end="")
