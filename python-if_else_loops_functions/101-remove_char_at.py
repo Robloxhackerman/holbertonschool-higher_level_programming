d007ce01bd801fd746ad471f035f993cfb2a9195
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-09-10 16:22:00 -0700

    try2

--- python-if_else_loops_functions/101-remove_char_at.py
+++ python-if_else_loops_functions/101-remove_char_at.py
@@ -3,5 +3,7 @@ def remove_char_at(str, n):
     PEPEXTO = ""
     for PEPE1 in range(len(str)):
         if PEPE1 == n:
+            continue
+        else:
             PEPEXTO = PEPEXTO + str[PEPE1]
     return PEPEXTO
