ae9c5330b2d940b2ba12d43b6b68e1cdb2988e51
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-09-10 16:35:10 -0700

    try asasadsd

---
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+for PEPE1 in range(0, 10):
+    for PEPE2 in range(0, 10):
+        if (PEPE1 != PEPE2 and PEPE1 < PEPE2) and PEPE1 < 9:
+            if PEPE1 == 8 and PEPE2 == 9:
+                print("{}{}".format(PEPE1, PEPE2))
+            else:
+                print("{}{}".format(PEPE1, PEPE2), end="")
