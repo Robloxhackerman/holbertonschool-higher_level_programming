32007fc3d1b295ce746f162cf13f6e920fa8f690
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-09-10 16:20:29 -0700

    try1

---
+++ python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def remove_char_at(str, n):
+    PEPEXTO = ""
+    for PEPE1 in range(len(str)):
+        if PEPE1 == n:
+            PEPEXTO = PEPEXTO + str[PEPE1]
+    return PEPEXTO
