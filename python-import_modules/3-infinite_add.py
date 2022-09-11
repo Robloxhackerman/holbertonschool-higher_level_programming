031192a45a9130bfd07cbdd9ddafe3db5573506f
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-09-10 19:16:38 -0700

    try6 big changes

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -5,11 +5,7 @@ if __name__ == "__main__":
     PEMENTO = sys.argv
     PEPUMA = 0
 
-    if PEPARGO == 0:
-        print("0")
-    elif PEPARGO == 1:
-        print("{}".format(PEMENTO))
-    elif PEPARGO > 0 and PEPARGO != 1:
+    elif PEPARGO > 1:
         for PEPE1 in range(1, PEPARGO):
             PEPUMA = PEPUMA + int(PEMENTO[PEPARGO])
-        print("{}".format(PEPUMA))
+    print("{}".format(PEPUMA))
