296501a3b5efd5f070549c94802bcf37a05ed839
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-09-10 22:34:20 -0700

    iluminacion divina?????

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3.4
-
+from hidden_4 import *
 if __name__ == "__main__":
-    from hidden_4 import *
-    for PEPE1 in dir():
-        if PEPE1[:2] != "__":
+    PEPE = dir(hidden_4)
+    for PEPE1 in range(len(PEPE)):
+        if PEPE[PEPE1][:2] != "__":
             print("{}".format(PEPE1))
