32f5ec274ed4a1f5d26e4ef06a6ae53d012e9acb
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-09-10 23:13:19 -0700

    try2 +

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -9,4 +9,5 @@ if __name__ == "__main__":
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
     else:
         if PEMENTO[2] == "+":
-            print("{} + {} = {}".format(PEMENTO[1], PEMENTO[3], add(PEMENTO[1], PEMENTO[3])))
+            PEPON = add(PEMENTO[1], PEMENTO[3])
+            print("{} + {} = {}".format(PEMENTO[1], PEMENTO[3], PEPON))
