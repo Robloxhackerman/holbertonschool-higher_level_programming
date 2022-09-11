3495736d149427f33b501da670c42ef97f9ee823
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-09-11 12:55:44 -0700

    try2 el tab

--- python-import_modules/102-magic_calculation.py
+++ python-import_modules/102-magic_calculation.py
@@ -6,6 +6,6 @@ def magic_calculation(a, b):
         c = add(a, b)
         for i in range(4, 6):
             c = add(c, i)
-            return c
-        else:
-            sub(a,b)
+        return c
+    else:
+        sub(a,b)
