edefef8d44011915077a549719d2c2073dc81c64
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-09-10 23:26:08 -0700

    try5

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -6,7 +6,8 @@ if __name__ == "__main__":
     PEMENTO = argv
 
     if PEPARGO != 3:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        print("./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
     else:
         if PEMENTO[2] == "+":
             print("{} + {} = {}".format(PEMENTO[1], PEMENTO[3], add(int(PEMENTO[1]), int(PEMENTO[3]))))
