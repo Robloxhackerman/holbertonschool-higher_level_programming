7d52f6d0c126b09d25053291ec61c8eb275995ff
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-09-10 22:17:34 -0700

    try hay que leer

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3.4
 from hidden_4 import *
 if __name__ == "__main__":
-    for PEPE1 in dir():
+    for PEPE1 in dir(hidden_4):
         if PEPE1[:2] != "__":
             print("{}".format(PEPE1))
