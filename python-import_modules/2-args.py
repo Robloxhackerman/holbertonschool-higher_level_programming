e9e596b45817c7b6c1188e15d63222545e0dc943
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-09-10 18:18:23 -0700

    probando 1..2..3

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
 import sys
 if __name__ == "__main__":
-    PEPARGO = len(sys.argv)
+    PEPARGO = len(sys.argv) - 1
+    PEMENTO = sys.argv
+
     if PEPARGO == 0:
         print("arguments.")
-    elif PEPARGO > 0:
+    elif PEPARGO == 1:
+        print("{} argument:".format(PEPARGO))
+        for PEPE1 in range(1, PEPARGO):
+            print("{}: {}".format(PEPE1, PEMENTO[PEPE1]))
+    elif PEPARGO > 0 and != 1:
         print("{} arguments:".format(PEPARGO))
