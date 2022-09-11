11cf83d0aaf98b81759e25146e404d158cb3acc1
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-09-10 23:06:05 -0700

    probando 1, 2, 3

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-from calculator_1.py import add, sub, mul, div
+from calculator_1 import add, sub, mul, div
 from sys import argv
 if __name__ == "__main__":
     PEPARGO = len(sys.argv)
