45b60c154a6b72f680edf3322a808fdac45cd4d4
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-09-10 19:37:31 -0700

    try12

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -6,6 +6,6 @@ if __name__ == "__main__":
     PEPUMA = 0
 
     if PEPARGO > 1:
-        for PEPE1 in range(1, PEPARGO):
+        for PEPE1 in range(1, PEPARGO - 1):
             PEPUMA = PEPUMA + int(PEMENTO[PEPARGO])
     print(PEPUMA)
