0def3af133efafa9771345a4024286c5fedd5aee
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-09-10 20:10:10 -0700

    a

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -4,4 +4,4 @@ if __name__ == "__main__":
     PEPUMA = 0
     for PEPE1 in range(1, len(sys.argv)):
         PEPUMA = PEPUMA + int(sys.argv[PEPE1])
-    print("{}".format(PEPUMA)) 
+    print("{}".format(PEPUMA))
