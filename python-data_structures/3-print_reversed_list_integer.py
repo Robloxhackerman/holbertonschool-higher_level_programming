538ccd321611164ede5879d91259937f050531dc
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-09-12 06:19:10 -0700

    big brain time

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    listita = my_list.reverse()
-    for PEPE1 in range(len(my_list)):
-        print("{:d}".format(listita[PEPE1]))
+    if my_list:
+        for PEPE1 in reversed(my_list):
+            print("{:d}".format(my_list[PEPE1]))
