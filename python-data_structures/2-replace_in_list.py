d604aba9f26c0f45ee0b307465fb284a61399c3b
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-09-12 06:08:26 -0700

    try3

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -5,5 +5,5 @@ def replace_in_list(my_list, idx, element):
     elif idx > len(my_list) - 1:
         return my_list
     else:
-        my_list[PEPE1] = element
+        my_list[idx] = element
         return my_list
