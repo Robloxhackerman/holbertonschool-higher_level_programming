927b7c1f9356bd63b45732cac0358f594de01b2b
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-09-12 06:50:25 -0700

    try3

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -2,7 +2,7 @@
 def no_c(my_string):
     listita = ""
     for PEPE1 in range(len(my_string)):
-        if my_string[PEPE1] == "c" or my_string[PEPE1] == "C":
+        if my_string[PEPE1] != "c" or my_string[PEPE1] != "C":
             listita = listita + my_string[PEPE1]
         else:
             continue
