93931264217c52e0d4ea97d57ad7bc7eaecf0ee8
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-09-12 06:20:43 -0700

    try4

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -2,4 +2,4 @@
 def print_reversed_list_integer(my_list=[]):
     if my_list:
         for PEPE1 in reversed(my_list):
-            print("{:d}".format(my_list[PEPE1]))
+            print("{:d}".format(my_list[PEPE1 - 1]))
