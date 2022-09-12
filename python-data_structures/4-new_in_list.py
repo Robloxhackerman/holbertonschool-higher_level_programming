077dc414f2657a117569d4535f64ca0487bbe706
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-09-12 06:33:12 -0700

    try2

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
+    listita = my_list.copy()
     if idx < 0:
-        return my_list
+        return listita
     elif idx > (len(my_list) - 1):
-        return my_list
+        return listita
     else:
-        listita = my_list
         listita[idx] = element
         return listita
