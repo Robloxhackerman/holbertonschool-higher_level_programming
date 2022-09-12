82a2be10f27de6d6d0f375ba775e7846584e2d0f
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-09-12 07:11:22 -0700

    try1

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    if matrix:
+        for PEPE1 in matrix:
+            PEPE2 = 1
+            PEPARGO = len(matrix)
+        for PEPE3 in PEPE1:
+            if PEPE2 == PEPARGO:
+                print("{:d}".format(PEPE2), end="")
+            else:
+                print("{:d}".format(PEPE2), end="")
+            PEPE2++
+        print()
