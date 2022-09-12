e87e1e91e41c04eb6bec58789a0ba2f62df14d4d
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-09-12 05:52:31 -0700

    try2

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
     for PEPE1 in range(len(my_list)):
-        print("{}".format(my_list[PEPE1]))
+        print("{:d}".format(my_list[PEPE1]))
