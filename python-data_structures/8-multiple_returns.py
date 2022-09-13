46e3dc9332386fc4062100b7444c839163bca9b0
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-09-13 06:09:54 -0700

    try1

---
+++ python-data_structures/8-multiple_returns.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def multiple_return(sentece):
+    if sentence:
+        tuplita = (len(sentence), sentence[1:],)
+        return tuplita
+    else:
+        tuplita = ("None", 0)
