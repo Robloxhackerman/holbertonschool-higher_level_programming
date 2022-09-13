9fc88fb329837c2b995132fb35f11861d663dba8
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-09-13 06:03:35 -0700

    siuuuuuuuuuuuuuuuuuuu

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -3,7 +3,7 @@ def add_tuple(tuple_a=(), tuple_b=()):
     if tuple_a and tuple_b:
         if len(tuple_a) == 1 and len(tuple_b) == 1:
             PEPUM1 = tuple_a[0] + tuple_b[0]
-            tuplita = (PEPUM1,)
+            tuplita = (PEPUM1, 0,)
             return tuplita
         elif len(tuple_a) < 2:
             PEPIN = (0,)
