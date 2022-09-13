faa3c63b9fb19d9d00b2560e1d7059cc57261f73
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-09-13 08:27:22 -0700

    try1.1

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def square_matrix_simple(matrix[]):
+def square_matrix_simple(matrix=[]):
     for PEPE1 in range(len(matrix)):
         for PEPE2 in range(len(matrix[PEPE1])
                 matrix[PEPE1][PEPE2] *= matrix[PEPE1][PEPE2]
