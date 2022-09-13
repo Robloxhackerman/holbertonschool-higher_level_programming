9a1cc1891a5d3e2c267acf10538ed300821e6fb2
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-09-13 06:54:42 -0700

    try8

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -2,8 +2,7 @@
 def divisible_by_2(my_list=[]):
     listita = []
     for PEPE1 in range(len(my_list)):
-        listita.append(None)
         if my_list[PEPE1] % 2 == 0:
-            listita[PEPE1] = True
+            listita.append(True)
         else:
-            listita[PEPE1] = False
+            listita.append(False)
