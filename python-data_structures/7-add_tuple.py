f391d5b9d8231179537226985f2e395e5cc14ad8
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-09-13 05:28:51 -0700

    try3

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    if len(tuple_a) < 2:
-        PEPIN = (0,)
-        for PEPE1 in range(len(tuple_a)):
-            tuple_a = tuple_a + PEPIN
-    elif len(tuple_b) < 2:
-        PEPIN = (0,)
-        for PEPE1 in range(len(tuple_b)):
-            tuple_b = tuple_b + PEPIN
+    if tuple_a:
+        if len(tuple_a) < 2:
+            PEPIN = (0,)
+            for PEPE1 in range(3):
+                tuple_a = tuple_a + PEPIN
+        elif len(tuple_b) < 2:
+            PEPIN = (0,)
+            for PEPE1 in range(3)):
+                tuple_b = tuple_b + PEPIN
 
-    PEPUM1 = tuple_a[0] + tuple_b[0]
-    PEPUM2 = tuple_a[1] + tuple_b[1]
-    tuplita = (PEPUM1, PEPUM2,)
-    return tuplita
+        PEPUM1 = tuple_a[0] + tuple_b[0]
+        PEPUM2 = tuple_a[1] + tuple_b[1]
+        tuplita = (PEPUM1, PEPUM2,)
+        return tuplita
