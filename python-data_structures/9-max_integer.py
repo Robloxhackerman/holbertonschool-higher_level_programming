8859505dae3c2d84be7c8c9414bdc77a31a4b4a1
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-09-13 06:30:57 -0700

    try5

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -2,7 +2,7 @@
 def max_integer(my_list=[]):
     if my_list:
         PEPOTE = 0
-        for PEPE1 in len(my_list) - 1:
+        for PEPE1 in range(len(my_list) - 1):
             if my_list[PEPE1] >= PEPOTE:
                 PEPOTE = my_list[PEPE1]
             else:
