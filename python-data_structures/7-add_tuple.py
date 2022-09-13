61f3eb51f40e9c6f40a64699eadec4f10211dbf7
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-09-13 05:25:37 -0700

    error con el index

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -10,6 +10,6 @@ def add_tuple(tuple_a=(), tuple_b=()):
             tuple_b = tuple_b + PEPIN
 
     PEPUM1 = tuple_a[0] + tuple_b[0]
-    PEPUM2 = tuple_b[0] + tuple_b[1]
+    PEPUM2 = tuple_a[1] + tuple_b[1]
     tuplita = (PEPUM1, PEPUM2,)
     return tuplita
