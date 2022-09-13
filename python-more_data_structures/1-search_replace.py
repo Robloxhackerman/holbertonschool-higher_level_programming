9385e88df392efc3fa5c21ced303dc9a01f655b0
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-09-13 08:55:18 -0700

    try1

---
+++ python-more_data_structures/1-search_replace.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    for PEPE1 in range(len(my_list):
+        if my_list[PEPE1] == search:
+            my_list[PEPE1] = search:
+        else:
+            continue
