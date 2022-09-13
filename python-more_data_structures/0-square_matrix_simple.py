1af8d34c8b14e74a5befb25d668d1a605a668aa7
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-09-13 08:44:01 -0700

    try3

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    for PEPE1 in range(len(matrix)):
-        for PEPE2 in range(len(matrix[PEPE1])
-                PILIN = matrix[PEPE1][PEPE2]
-                matrix[PEPE1][PEPE2] = PILIN
-    return matrix
-
+    matrisita = []
+    for PEPE in matrix[]:
+        matrisita.append(list(map(lambda x : x ** 2, PEPE)))
+    return matrisita
