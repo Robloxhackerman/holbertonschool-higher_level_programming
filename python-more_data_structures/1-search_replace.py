8140eea1ef56fbcba0f39fd0a06089b94ac7291d
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-09-13 08:57:38 -0700

    que bobo

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -2,6 +2,6 @@
 def search_replace(my_list, search, replace):
     for PEPE1 in range(len(my_list)):
         if my_list[PEPE1] == search:
-            my_list[PEPE1] = search:
+            my_list[PEPE1] = search
         else:
             continue
