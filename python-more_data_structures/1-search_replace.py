6b0420a94abb8a50c4f2adf412cf8a9a206bb788
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-09-13 09:02:09 -0700

    try5

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -2,6 +2,7 @@
 def search_replace(my_list, search, replace):
     for PEPE1 in range(len(my_list)):
         if my_list[PEPE1] == search:
-            my_list[PEPE1] = search
+            my_list.replace(search, search)
         else:
             continue
+    return my_list
