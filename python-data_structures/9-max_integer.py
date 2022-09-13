99324a67dca312ef975f662974f198916509cfd4
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-09-13 07:20:34 -0700

    nasty

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     if my_list:
-        PEPOTE = 0
+        PEPOTE = -10000
         for PEPE1 in range(len(my_list)):
             if my_list[PEPE1] >= PEPOTE:
                 PEPOTE = my_list[PEPE1]
