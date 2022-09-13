0be9d8c83d1d1d7a743935ff7b04e08dcc1cbb1a
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-09-13 08:28:58 -0700

    try2

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -2,6 +2,7 @@
 def square_matrix_simple(matrix=[]):
     for PEPE1 in range(len(matrix)):
         for PEPE2 in range(len(matrix[PEPE1])
-                matrix[PEPE1][PEPE2] *= matrix[PEPE1][PEPE2]
+                PILIN = matrix[PEPE1][PEPE2]
+                matrix[PEPE1][PEPE2] = PILIN
     return matrix
 
