f0366669cec4df74dd37d1b6279766232350f876
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-09-13 06:13:44 -0700

    try4

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if sentence:
-        tuplita = (len(sentence), sentence[1:2],)
+        tuplita = (len(sentence), sentence[0:1],)
         return tuplita
     else:
         tuplita = ("None", 0)
