e13885a77764ba7abe698fc0d710707d0572af1d
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-09-13 08:45:37 -0700

    python_sama

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -2,5 +2,5 @@
 def square_matrix_simple(matrix=[]):
     matrisita = []
     for PEPE in matrix[:]:
-        matrisita.append(list(map(lambda x : x ** 2, PEPE)))
+        matrisita.append(list(map(lambda x: x ** 2, PEPE)))
     return matrisita
