03bf012ba34e9777d879e1dcabef23a547077259
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-09-14 05:43:51 -0700

    try3, leyendo

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    PEPE = 0
-    for PEPE1 in range(len(my_list)):
-        aca_ta = my_list.count(my_list[PEPE1])
-        if  aca_ta == 1:
-            PEPE = PEPE + my_list[PEPE1]
-    return PEPE
+    return sum(set(my_list))
