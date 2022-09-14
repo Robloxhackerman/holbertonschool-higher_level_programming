d46e812f30860705975bf3a8649217ebc9a9aa79
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-09-14 07:12:07 -0700

    try2

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -2,8 +2,9 @@
 def best_score(a_dictionary):
     PEPON = 0
     PEPONCORE = 0
-    for PEPE1, PEPE2 in a_dictionary.items():
-        if PEPE2 >= PEPONCORE:
-            PEPONCORE = PEPE2
-            PEPON = PEPE1
+    if a_dictionary:
+        for PEPE1, PEPE2 in a_dictionary.items():
+            if PEPE2 >= PEPONCORE:
+                PEPONCORE = PEPE2
+                PEPON = PEPE1
     return PEPON
