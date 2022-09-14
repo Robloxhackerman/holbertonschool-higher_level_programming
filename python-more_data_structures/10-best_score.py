a53bd3dbb6948a629b578562271f5e291ad5c866
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-09-14 07:09:39 -0700

    try1.1

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -3,5 +3,5 @@ def best_score(a_dictionary):
     PEPON = 0
     for PEPE1, PEPE2 in a_dictionary.items():
         if PEPE2 >= PEPON:
-            PEPON = PEPE2
+            PEPON = PEPE1
     return PEPON
