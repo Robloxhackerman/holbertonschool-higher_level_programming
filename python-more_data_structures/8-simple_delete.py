a6d4c448cdbc53f1587f2fcb25529f0bd356c82c
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-09-14 06:44:42 -0700

    try4

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    if a_dictionary[key]:
+    if key in a_dictionary:
         del a_dictionary[key]
-    else:
-
     return a_dictionary
