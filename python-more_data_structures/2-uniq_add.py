875004154e5258aae26ee4f21ba6d70110d82166
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-09-14 05:31:02 -0700

    try2

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -2,9 +2,7 @@
 def uniq_add(my_list=[]):
     PEPE = 0
     for PEPE1 in range(len(my_list)):
-        for PEPE2 in range(len(my_list)):
-            if my_list[PEPE1] == my_list[PEPE2]:
-                continue
-            else:
-                PEPE = PEPE + my_list[PEPE1]
+        aca_ta = my_list.count(my_list[PEPE1])
+        if  aca_ta == 1:
+            PEPE = PEPE + my_list[PEPE1]
     return PEPE
