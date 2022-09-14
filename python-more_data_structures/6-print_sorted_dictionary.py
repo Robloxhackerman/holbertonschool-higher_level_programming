88b793f1e7593f013f1b66ac215d18d63fa7ab9f
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-09-14 06:31:45 -0700

    try3.1 parentesis

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,4 +2,4 @@
 def print_sorted_dictionary(a_dictionary):
     diccionarito = sorted(a_dictionary.items())
     for PEPE1, PEPE2 in diccionarito:
-        print("{}: {}".format(PEPE1, PEPE2)
+        print("{}: {}".format(PEPE1, PEPE2))
