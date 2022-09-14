c2a942c01facf65d6823a58361abb62432b09efc
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-09-14 06:58:32 -0700

    try1

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -2,5 +2,5 @@
 def multiply_by_2(a_dictionary):
     diccionarito = a_dictionary.copy()
     for PEPE1, PEPE2 in diccionarito.items():
-        diccionarito = PEPE2 * 2
+        diccionarito[PEPE1] = PEPE2 * 2
     return diccionarito
