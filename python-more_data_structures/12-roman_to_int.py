8961d51767d934192229be35c5b170b475553d59
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-09-14 18:03:53 -0700

    try0 probando

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,9 +2,6 @@
 def roman_to_int(roman_string):
     if roman_string is not str or roman_string is None:
         return 0
-    romanitos = [["I", 1], ["V", 5], ["X", 10], ["L", 50],
-            ["C", 100], ["D", 500], ["M", 1000]]
-
-    for PEPE1 in roman_string:
-        for PEPE2 in romanitos:
-            if PEPE1 == PEPE1[0]:
+    romanitos = {"I":1, "V":5, "X":10, "L":50,
+            "C":100, "D":500, "M":1000}
+    print(romanitos)
