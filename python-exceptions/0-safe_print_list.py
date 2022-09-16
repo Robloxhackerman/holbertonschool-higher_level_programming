e6710947d6d11926eaa170939d0523ece3d40187
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-09-16 07:19:52 -0700

    try1.1

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -2,7 +2,7 @@
 def safe_print_list(my_list=[], x=0):
     try:
         for PEPE1 in range(x):
-            print("{}".format(PEPE1), end="")
+            print("{:d}".format(PEPE1), end="")
         print()
     except:
         print("mal pibin")
