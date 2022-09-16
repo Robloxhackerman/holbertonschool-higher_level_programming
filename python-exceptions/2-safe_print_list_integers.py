b145233d87f2cddffd8dff26fa151400e02dcaa2
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-09-16 08:34:19 -0700

    try1.5

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -9,6 +9,7 @@ def safe_print_list_integers(my_list=[], x=0):
                 PEPIN = PEPIN + 1
             else:
                 continue
+        print()
     except:
         pass
     finally:
