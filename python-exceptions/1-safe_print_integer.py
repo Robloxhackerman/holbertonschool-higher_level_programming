d22ac92c64d4c21ff8504ce8716c1efb50761958
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-09-16 08:02:13 -0700

    try1.1

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        print("{:d}".format(value))
+        if value:
+            print("{:d}".format(value))
     except TypeError:
         return False
     finally:
