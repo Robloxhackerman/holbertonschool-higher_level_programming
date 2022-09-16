160b37fb567e71a1d82704c4d70c174ac4f313a3
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-09-16 07:26:48 -0700

    try1.2

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -4,7 +4,7 @@ def safe_print_list(my_list=[], x=0):
     try:
         for PEPE1 in my_list:
             if PEPIN < x:
-                print("{}".format(PEPE1), end="")
+                print("{}".format(my_list[PEPIN]), end="")
             PEPIN = PEPIN + 1
         print()
     except:
