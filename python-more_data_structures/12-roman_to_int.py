23446dae64e82c96fc78ff8faa6d0e2d0328cc62
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-09-16 05:35:41 -0700

    try1.1

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,8 +2,8 @@
 def roman_to_int(roman_string):
     if roman_string is not str or roman_string is None:
         return 0
-    romanitos = {"I":1, "V":5, "X":10, "L":50,
-            "C":100, "D":500, "M":1000}
+    romanitos = {"I": 1, "V": 5, "X": 10, "L": 50,
+            "C": 100, "D": 500, "M": 1000}
     JUNO = romanitos.values(roman_string[0])
     for JUPITER in roman_string:
         MARTE = romanitos.values(roman_string[JUPITER])
