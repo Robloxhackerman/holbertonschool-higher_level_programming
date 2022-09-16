2e5fd87a6608ead996ab336c6ffaa46efcbbc891
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-09-16 08:31:10 -0700

    try1.2

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
         for PEPE1 in my_list:
             if PEPIN < x:
                 if isinstance(PEPE1, int):
-                    print("{:d".format(PEPUM))
+                    print("{:d}".format(PEPIN))
                 PEPIN = PEPIN + 1
             else:
                 continue
