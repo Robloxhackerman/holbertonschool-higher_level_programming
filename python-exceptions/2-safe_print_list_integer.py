868a42c11429c3baf2f95d89dd03bc8359b086cd
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-09-16 08:27:42 -0700

    try1

---
+++ python-exceptions/2-safe_print_list_integer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    PEPIN = 0
+    PEPUM = 0
+    try:
+        for PEPE1 in my_list:
+            PEPUM = PEPE1
+            if PEPIN < x:
+                if isinstance(PEPE1, int):
+                    print("{:d".format(PEPUM))
+                PEPIN = PEPIN + 1
+            else:
+                continue
+    except TypeError:
+        pass
+    finally:
+        return 
