0053bc233c45067fcc3dc48c29688c6bb680b849
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-09-16 08:08:12 -0700

    try2.2 nasty??

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        if value:
+        if value != None:
             print("{:d}".format(value))
+        else:
+            return False
     except:
         return False
     else:
