6af66cd06441a476cf51ecd78e086d9c21ddd9c9
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-09-16 06:13:38 -0700

    try2.2

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -3,10 +3,11 @@ def roman_to_int(roman_string):
     romanitos = {"I": 1, "V": 5, "X": 10, "L": 50,
             "C": 100, "D": 500, "M": 1000}
     JUNO = 0
+    DIANA = 0
     for JUPITER in range(len(roman_string)):
         MARTE = romanitos[roman_string[JUPITER]]
         if JUPITER == len(roman_string) - 1:
-            continue
+            DIANA = DIANA
         else:
             DIANA = romanitos[roman_string[JUPITER + 1]]
         if MARTE < DIANA:
