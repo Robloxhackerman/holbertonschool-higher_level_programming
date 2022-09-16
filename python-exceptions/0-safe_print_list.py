f1a251eba22c454f9446cfcc57e56cb8f50ccd93
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-09-16 07:32:01 -0700

    try2.1 el tab

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
         for PEPE1 in my_list:
             if PEPIN < x:
                 print("{}".format(my_list[PEPIN]), end="")
-            PEPIN = PEPIN + 1
+                PEPIN = PEPIN + 1
         print()
     except TypeError:
         pass
