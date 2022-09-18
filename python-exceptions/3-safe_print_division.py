0cd44a94d3d9c8ca97e10a008d1e88bb50111a6e
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-09-18 13:00:35 -0700

    trty2.2 xd el format

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -6,5 +6,5 @@ def safe_print_division(a, b):
     except (TypeError, ZeroDivisionError):
         PEPE = None
     finally:
-        print("Inside result: {:.1f}".format(PEPE))
+        print("Inside result: {}".format(PEPE))
         return PEPE
