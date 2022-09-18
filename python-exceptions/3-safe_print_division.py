79e6d70543dba16310fb7034611e7b78964e3cbc
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-09-18 12:56:11 -0700

    try1.2

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -3,7 +3,7 @@ def safe_print_division(a, b):
     PEPE = 0
     try:
         PEPE = a / b
-    except ValueError:
+    except:
         PEPE = None
     finally:
         print("Inside result: {:.1f}".format(PEPE))
