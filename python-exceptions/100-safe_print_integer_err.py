0d87197cf33f394ef9b7e2d766a734aeebf927f0
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-09-18 13:51:10 -0700

    try2.1

--- python-exceptions/100-safe_print_integer_err.py
+++ python-exceptions/100-safe_print_integer_err.py
@@ -3,7 +3,7 @@ from sys import stderr
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
-    except (ValueError, TypeError) te:
+    except (ValueError, TypeError) as te:
         stderr.write("Exception: {}".format(te))
         return False
     return True
