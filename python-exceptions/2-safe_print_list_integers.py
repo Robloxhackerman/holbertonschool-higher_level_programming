494a6087c6ca7a9c38380c04124511523f026e6c
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-09-18 12:41:29 -0700

    try3.1 !

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -6,7 +6,7 @@ def safe_print_list(my_list=[], x=0):
             print("{:d}".format(my_list[PEPE1]), end="")
             PEPIN = PEPIN + 1
         except (ValueError, TypeError):
-            PEPE! = PEPE1 + 1
+            PEPE1 = PEPE1 + 1
             continue
     print()
     return PEPIN
