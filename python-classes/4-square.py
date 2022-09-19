c48eec49dc42d59559a98f166a68a52e77c713dc
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-09-19 09:43:14 -0700

    try1.3

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -22,6 +22,10 @@ class Square:
             size (int): size of square
 
         """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
         self.__size = size
     def size(self):
         """size
