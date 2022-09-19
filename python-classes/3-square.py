821edcf00c0a7b35b17a2753263be2975b2245c5
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-09-19 08:51:33 -0700

    try1.1

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -7,12 +7,31 @@ define a square
 
 
 class Square:
+    """Square
+
+    a square
+
+    """
+
     def __init__(self, size=0):
+        """__init__
+
+        initialize a square
+
+        Attributes:
+            size (int): size of square
+
+        """
         if type(size) is not int:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
         sel.__size = size
     def area(self):
+        """area
+
+        calculate the area of a square
+
+        """
         area = self.size * self.size
         return area
