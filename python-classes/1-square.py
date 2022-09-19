b58af6005ba4b879c6fdcce9d569ea66e6cb9753
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-09-19 06:46:45 -0700

    try1

---
+++ python-classes/1-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python2
+"""Class Square
+
+defines a square
+
+"""
+
+
+class Square(self, size):
+    
+    def __init__(self, size):
+        """__init__
+
+        The __init__ method inlitialize the value
+
+        Args:
+            size (int): the size.
+
+        """
+        self.__size
