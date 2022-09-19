09c023045cda697612bcd4b18aa2f61d86341377
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-09-19 08:52:18 -0700

    try1.2

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -26,7 +26,7 @@ class Square:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
-        sel.__size = size
+        self.__size = size
     def area(self):
         """area
 
