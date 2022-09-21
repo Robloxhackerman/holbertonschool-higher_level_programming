ee66623dd4695a70c93dae2312f15bfcab1609b4
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-09-21 07:25:01 -0700

    try1

---
+++ python-classes/5-square.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python3
+"""Square Class
+
+define a square
+
+"""
+
+
+class Square:
+    """Square
+
+    a square
+
+    """
+
+    def __init__(self, size=0):
+        """__init__
+
+        initialize a square
+
+        Attributes:
+            size (int): size of square
+
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.size = size
+
+    @property
+    def size(self):
+        """size
+
+        get the size
+
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """size
+
+        sets the size
+
+        """
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """area
+
+        calculate the area of a square
+
+        """
+        area = self.__size ** 2
+        return area
+
+    def my_print(self):
+        """my_print
+
+        prints the area
+
+        """
+        for PEPE1 in range(self.__size):
+            for PEPE2 in range(self.__size):
+                print("#", end="")
+            print()
