b834617a4b50313789b1172d709a0b45da49818c
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-09-22 05:27:37 -0700

    try1

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,5 +1,18 @@
 #!/usr/bin/python3
 def add_integer(a, b=98):
+    """sumeishon
+
+    adds 2 integers
+
+    Args:
+        a (int): value
+        b (int): value
+
+    Return:
+        a + b
+
+    """
+
     PEPE = 0
 
     if type(a) is not int or type(a) is not float:
