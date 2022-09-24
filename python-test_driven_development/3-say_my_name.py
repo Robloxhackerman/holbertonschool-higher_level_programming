1a9c9513287959483ce5f027533cf18290ee7e66
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-09-23 18:56:42 -0700

    try2

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -5,6 +5,8 @@
 def say_my_name(first_name, last_name=""):
     """ Prints a name"""
 
-    if first_name is not str or last_name is not str:
-        raise TypeError("first_name must be a string or last_name must be a string")
+    if first_name is not str:
+        raise TypeError("first_name must be a string")
+    if last_name is not str:
+        raise TypeError("last_name must be a string")
     print("My name is {} {}".format(first_name, last_name))
