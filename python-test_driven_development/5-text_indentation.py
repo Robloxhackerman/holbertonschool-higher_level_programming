8fd5895d1182d6c489aabeff6edf29388af4d4e1
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-09-24 19:28:52 -0700

    try1

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Python-matron"""
+
+
+def text_indentation(text):
+    """ prints a text with 2
+    new lines after each of 
+    these characters: ., ? and :
+    """
+
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+    PEPE = False
+    PEPIN = 0
+    for PEPE1 in range(len(text)):
+        if text[PEPE1] is in ".?:":
+            PEPE = False
+            PEPON = PEPE1 + 1
+            print(text[PEPIN:PEPON].lstrip(), end='\n\n')
+            PEPIN = PEPON
+        if PEPE is True and PEPE1 == len(text) - 1:
+            print(text[PEPIN:].lstrip(), end="")
+
+    if PEPE is False:
+        print(text.lstrip(), end="")
