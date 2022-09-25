f03c3c8fda3263dd48d85eb2fcd7914fd50f1a1e
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-09-25 15:13:20 -0700

    try3.2

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -23,7 +23,7 @@ class Rectangle:
         return PEPE
 
         def __repr__(self):
-            return repr('Rectangle(' + self.__width + ', ' + self.__height )
+            return repr('Rectangle(' + self.width + ', ' + self.height)
 
     @property
     def width(self):
