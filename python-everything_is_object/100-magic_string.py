b166b3c9a299d027050e7866ebf1432908d5d21b
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-09-25 19:43:08 -0700

    try5 xd

--- python-everything_is_object/100-magic_string.py
+++ python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def magic_string(PEPE=[0]):
     PEPE[0] += 1
-    return str("Holberton, " * (PEPE[0] - 1)) "Holberton"
+    return str("Holberton, " * (PEPE[0] - 1)) + "Holberton"
