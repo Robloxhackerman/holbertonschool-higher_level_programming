3accedfda299f394fb181e7628169f65936614cf
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-09-25 19:18:10 -0700

    try4

--- python-test_driven_development/100-matrix_mul.py
+++ python-test_driven_development/100-matrix_mul.py
@@ -10,7 +10,13 @@ def matrix_mul(m_a, m_b):
     
     if type(m_b) is not list:
         raise TypeError("m_b must be a list")
-    
+
+    if len(m_a) == 0 or type(m_a[0]) is list and len(m_a[0]) == 0:
+        raise ValueError("m_a can't be empty")
+
+    if len(m_b) == 0 or type(m_b[0]) is list and len(m_b[0]) == 0:
+        raise ValueError("m_b can't be empty")
+
     for PEPE1 in m_a:
         if not isinstance(PEPE1, list):
             raise TypeError("m_a must be a list of lists")
@@ -19,7 +25,7 @@ def matrix_mul(m_a, m_b):
                 raise TypeError("m_a should contain only integers or floats")
         if len(PEPE1) != len(m_a[0]):
                 raise TypeError("each row of m_a must be of the same size")
-    
+
     for PEPE3 in m_b:
         if not isinstance(PEPE2, list):
             raise TypeError("m_a must be a list of lists")
@@ -28,12 +34,6 @@ def matrix_mul(m_a, m_b):
                 raise TypeError("m_b should contain only integers or floats")
         if len(PEPE3) != len(m_b[0]):
             raise TypeError("each row of m_b must be of the same size")
-    
-    if len(m_a) == 0 or type(m_a[0]) is list and len(m_a[0]) == 0:
-        raise ValueError("m_a can't be empty")
-    
-    if len(m_b) == 0 or type(m_b[0]) is list and len(m_b[0]) == 0:
-        raise ValueError("m_b can't be empty")
 
     if len(m_a[0]) != len(m_b):
         raise ValueError("m_a and m_b can't be multiplied")
