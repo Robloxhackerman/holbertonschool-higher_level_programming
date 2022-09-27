111ea1684a65ea357bcd81fae84860415217c9b6
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-09-27 07:30:43 -0700

    try1

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""QUe nos deparara el futuro?"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """A rectangle"""
+
+    def __init__(self, size):
+        """Constructor"""
+
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def __str__(self):
+        return '[Square] ' + str(self.__size) + '/' + str(self.__size)
