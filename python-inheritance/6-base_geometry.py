ddc5730b27f5e0698f024be400369cbabeccc469
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-09-27 05:36:50 -0700

    try1

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""QUe nos deparara el futuro?"""
+
+
+class BaseGeometry():
+    """An empty class"""
+
+    def area(self):
+        """Raise an error"""
+
+        raise Exception("area() is not implemented")
