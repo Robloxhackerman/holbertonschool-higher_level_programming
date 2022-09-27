74ecaffc4deecbc8461add95e88e18c552ea1757
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-09-27 10:26:35 -0700

    try1

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Create object from a JSON file"""
+
+import json
+
+
+def load_from_json(filename):
+    """Create object from a JSON file"""
+    with open(filename) as f
+    return json.load(f)
+
