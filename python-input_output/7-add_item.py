a10ceff48137e8f85710d43dc146167cb0a90703
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-09-27 11:08:34 -0700

    try1

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Load, add, save"""
+
+import json
+import os.path
+import sys
+
+save_to_json_file = __import__("7-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("8-load_from_json_file").load_from_json_file
+
+filesito = "add_item.json"
+
+listita = []
+
+if os.path.exists(filesito) and os.path.getsize(filesito) > 0:
+    listita = load_from_json_files(filesito)
+
+if len(sys.argv) > 1:
+    for PEPE1 in sys.argv[1:]:
+        listita.append(PEPE1)
+
+save_to_json_file(listita, filesito)
