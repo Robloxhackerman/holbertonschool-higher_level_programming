af12797a578fa67d3fc492934631b3501aaf0577
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-09-27 15:39:47 -0700

    try2

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -17,7 +17,7 @@ class Student:
 
         PEPO = dict()
 
-        if type(attrs) si list and all(type(PEPE1) is str for PEPE1 in attrs):
+        if type(attrs) is list and all(type(PEPE1) is str for PEPE1 in attrs):
             for PEPE1 in attrs:
                 if PEPE1 in self.__dict__:
                     PEPO.update({x: self.__dict__[x]})
