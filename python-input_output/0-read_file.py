36c04b0dff0479541018d8ecaa553a0772615906
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-09-27 08:55:42 -0700

    try1

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+
+def read_file(filename=""):
+    """Reads a file and prints it to stdout"""
+
+    with open(filename, encoding="utf-8") as f:
+        PEPE = f.read()
+        print(PEPE, end="")
