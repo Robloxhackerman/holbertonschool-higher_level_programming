94f48f00312187733eefc29d0f3fe2057cd0c6ba
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-09-27 06:59:01 -0700

    try1

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""QUe nos deparara el futuro?"""
+
+
+class BaseGeometry():
+    """An empty class"""
+
+    def area(self):
+        """Raise an error"""
+
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """validates value"""
+
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+class Rectangle(BaseGeomtry):
+    """A rectangle"""
+
+    def __init__(self, width, height):
+        """Constructor"""
+
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
