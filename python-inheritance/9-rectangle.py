ecebc06a3e70571d998b01c56ab27f6c69c7a9e2
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-09-27 07:12:19 -0700

    try1

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""QUe nos deparara el futuro?"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """A rectangle"""
+
+    def __init__(self, width, height):
+        """Constructor"""
+
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def __str__(self):
+        return "[Rectangle] " + str(self.__width) + "/" + str(self.__height)
+
+    def area(self):
+        return self.__width * self.__height
