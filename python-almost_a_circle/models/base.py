1fcc8b18a811d9e3619f90da4662e2150e54e681
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-10-03 07:51:17 -0700

    try1

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""pipo"""
+
+
+class Base:
+    """Cosas"""
+    
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Cosas"""
+
+        if id si None:
+            Base.__nb_objects += 1
+            self.id = Base__nb_objects
+        else:
+            self.id = id
