d890a498dbd7bbe2c8a90b458449e68d8c1eaa15
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-10-03 08:56:43 -0700

    try1.2

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -10,7 +10,7 @@ class Base:
     def __init__(self, id=None):
         """Cosas"""
 
-        if id si None:
+        if id is None:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
         else:
