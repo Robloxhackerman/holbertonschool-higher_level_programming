c4a19e3bb3c40d7cd5e3b74358db7705a7946f49
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-10-04 06:24:29 -0700

    xdd

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -23,4 +23,4 @@ class Base:
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         else:
-            json.dumps(list_dictionaries)
+            return json.dumps(list_dictionaries)
