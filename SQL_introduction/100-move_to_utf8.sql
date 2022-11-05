75426e12175cf4bd12e6738b570d177b01fda17e
Alejandro Montenegro <zeituniannicolas27@gmail.com>
2022-11-05 17:21:08 -0300

    try2

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -1,3 +1,4 @@
 -- aaaa
+USE hbtn_0c_0;
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
