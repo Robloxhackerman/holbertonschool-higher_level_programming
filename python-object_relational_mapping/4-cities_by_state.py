14a6d4d4ab0fd67e0dc978867f93980c87fffadc
Alejandro Montenegro <zeituniannicolas27@gmail.com>
2022-11-13 14:13:11 -0300

    fix

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
         password=argv[2],
         database=argv[3]
         )
-    scursor = dataB.cursor()
+    cursor = dataB.cursor()
     cursor.execute("""SELECT cities.id, cities.name, states.name
     FROM cities JOIN states ON cities.state_id = states.id
     ORDER BY cities.id ASC""")
