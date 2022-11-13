45c96719c77cfa5d203dfde8d08a293dd316aad6
Alejandro Montenegro <zeituniannicolas27@gmail.com>
2022-11-13 14:37:16 -0300

    try3

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -6,24 +6,24 @@ from sys import argv
 
 """SQL Query"""
 if __name__ == "__main__":
-    dataB = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=argv[1],
-        password=argv[2],
-        database=argv[3]
-        )
+    try:
+        db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
+                             passwd=argv[2], db=argv[3])
+    except Exception:
+        print("Error")
 
-    cursor = dataB.cursor()
-    cursor.execute("SELECT cit.name FROM cities cit \
-        LEFT JOIN states st ON cit.st_id = s.id \
-        WHERE s.name = %s \
-        ORDER BY c.id", (argv[4],))
-    result = cursor.fetchall()
-    for i, record in enumerate(result):
-        if i > 0:
-            print(', ', end='')
-        print(str(record[0]), end='')
+    cursor = db.cursor()
+    cursor.execute("""SELECT cities.name FROM cities \
+                    JOIN states ON  cities.state_id = states.id WHERE \
+                    BINARY states.name = %s""", (argv[4],))
+    cont = cursor.fetchall()
+
+    for i in cont:
+        for j in i:
+            if i == cont[0]:
+                print(j, end='')
+            else:
+                print(f", {j}", end='')
     print()
     cursor.close()
-    dataB.close()
+    db.close()
