4e039ba6c9044608c2a74bbb723df552afeb4011
Alejandro Montenegro <zeituniannicolas27@gmail.com>
2022-11-13 14:53:55 -0300

    try1

---
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""aaaa"""
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == '__main__':
+    """aaaaa"""
+    try:
+        engine = create_engine(
+            'mysql+mysqldb://{}:{}@localhost:3306/{}'
+            .format(argv[1], argv[2], argv[3]), pool_pre_ping=True
+        )
+    except Exception:
+        print("Error")
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    ses = Session()
+    for i in ses.query(State).order_by(State.id).all():
+        print(f"{i.id}: {i.name}")
+    ses.close()
