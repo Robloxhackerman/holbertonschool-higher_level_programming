8089d8d0d5b039516380073692c8394636694b06
Alejandro Montenegro <zeituniannicolas27@gmail.com>
2022-12-06 13:49:31 -0300

    try1

--- 0-square_matrix_simple.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-def square_matrix_simple(matrix[]):
-    for PEPE1 in range(len(matrix)):
-        for PEPE2 in range(len(matrix[PEPE1])
-                matrix[PEPE1][PEPE2] *= matrix[PEPE1][PEPE2]
-    return matrix
-
