d108f429b4146dd413f038de3021f3a3cdd282ac
Alejandro Montenegro <zeituniannicolas27@gmail.com>
2022-12-07 10:40:59 -0300

    try1

---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+const ARREGLON = [];
+ARREGLON.push('C is fun', 'Python is cool', 'JavaScript is amazing');
+for (let PEPE1 = 0; PEPE1 < ARREGLON.length; PEPE1++) {
+  console.log(ARREGLON[PEPE1]);
+}
