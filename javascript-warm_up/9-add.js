341c957af6087253a2aa46548cc1490e8af9d4e9
Alejandro Montenegro <zeituniannicolas27@gmail.com>
2022-12-07 12:03:04 -0300

    fix

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-function add(a, b) {
-    return a + b;
+function add (a, b) {
+  return a + b;
 }
-let PAPOTE = add(parseInt(process.argv[2]), parseInt(process.argv[3]));
+const PAPOTE = add(parseInt(process.argv[2]), parseInt(process.argv[3]));
 console.log(PAPOTE);
