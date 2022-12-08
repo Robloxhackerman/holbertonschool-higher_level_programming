9874d8d8a539b1b59939e82c851af1ebad15a87f
Alejandro Montenegro <zeituniannicolas27@gmail.com>
2022-12-08 14:13:47 -0300

    try1

---
+++ javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+exports.nbOccurences = function (list, searchElement){
+    let CSGO = 0;
+    for (let PEPE1 = 0; PEPE1 < list.length; PEPE1++) {
+        if (list[PEPE1] === searchElement){
+            ++CSGO;
+        }
+    }
+    return CSGO;
+}
