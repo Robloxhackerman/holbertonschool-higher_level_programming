4e2d438f5592cccf31dd86dae1b9ab1506030df7
Alejandro Montenegro <zeituniannicolas27@gmail.com>
2022-12-08 14:02:16 -0300

    fix style

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 const Squirt = require('./5-square');
-module.exports = class Square extends Squirt{
-    constructor(size){
-        super(size, size);
-    }
+module.exports = class Square extends Squirt {
+  constructor (size) {
+    super(size, size);
+  }
 
-    charPrint(c='X'){
-        super.print(c)
-    }
+  charPrint (c = 'X') {
+    super.print(c);
+  }
 };
