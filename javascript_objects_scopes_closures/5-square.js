ca8589da6e4d59f0b46426f2cdd1aa23da6604c1
Alejandro Montenegro <zeituniannicolas27@gmail.com>
2022-12-08 13:37:22 -0300

    fix style

--- javascript_objects_scopes_closures/5-square.js
+++ javascript_objects_scopes_closures/5-square.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
-const Rectangle = require("./4-rectangle");
+const Rectangle = require('./4-rectangle');
 
-module.exports = class Square extends Rectangle{
-    constructor(size) {
-        super(size, size);
-    }
-}
+module.exports = class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+};
