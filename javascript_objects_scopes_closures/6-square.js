6a9e668bc0c65783f4f54e10ba348fe2923017c8
Alejandro Montenegro <zeituniannicolas27@gmail.com>
2022-12-08 13:59:33 -0300

    try3

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
-
-const Rectangle = require("./4-rectangle");
-
-module.exports = class Square extends Rectangle{
+const Square = require('./5-square');
+module.exports = class Square extends Square{
     constructor(size){
         super(size, size);
     }
