b468c7df86ba81b62a3dfbb867d9632b3051a244
Alejandro Montenegro <zeituniannicolas27@gmail.com>
2022-12-13 10:24:01 -0300

    try1

---
+++ javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const request = require('request');
+
+request(process.argv, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  }
+  console.log('code: ' + response.statusCode);
+});
