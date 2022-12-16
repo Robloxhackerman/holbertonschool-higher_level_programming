71830317b370ed3780340cb0795cb475d1331a8e
Alejandro Montenegro <zeituniannicolas27@gmail.com>
2022-12-16 11:11:12 -0300

    try1

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -1,20 +1,28 @@
 #!/usr/bin/node
 
-const request = require('request')
+const request = require('request');
 const arg = process.argv;
 let info = '';
-let mapi = new Map();
-let counter = 0;
+const mapi = {};
 
-request(arg[2], function(error, response, body) {
-    info = JSON.parse(body);
+request(arg[2], function (error, response, body) {
+  if (error) {
+    console.log(error);
+  }
+  info = JSON.parse(body);
 
-    
+  for (let PEPE1 = 0; PEPE1 < info.length; PEPE1++) {
+    const useMe = info[PEPE1].userId;
 
-    for (const x of info) {
-        if (x.completed === true) {
-            mapi.set(x.userId, mapi.get(x.userId) + 1 )
+    mapi[useMe] = 0;
+
+    for (let PEPE2 = 0; PEPE2 < info.length; PEPE2++) {
+      if (info[PEPE1].userId === info[PEPE2].userId) {
+        if (info[PEPE2].completed === true) {
+          mapi[useMe] = mapi[useMe] + 1;
         }
+      }
     }
-    console.log(mapi)
+  }
+  console.log(mapi);
 });
