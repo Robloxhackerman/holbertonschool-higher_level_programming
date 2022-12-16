24f49030e090cf53886684d3624979b50a8867c5
Alejandro Montenegro <zeituniannicolas27@gmail.com>
2022-12-16 11:23:36 -0300

    try2

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -14,15 +14,18 @@ request(arg[2], function (error, response, body) {
   for (let PEPE1 = 0; PEPE1 < info.length; PEPE1++) {
     const useMe = info[PEPE1].userId;
 
-    mapi[useMe] = 0;
+    if (info[PEPE1].completed) {
+      mapi[useMe] = 0;
 
-    for (let PEPE2 = 0; PEPE2 < info.length; PEPE2++) {
-      if (info[PEPE1].userId === info[PEPE2].userId) {
-        if (info[PEPE2].completed === true) {
-          mapi[useMe] = mapi[useMe] + 1;
+      for (let PEPE2 = 0; PEPE2 < info.length; PEPE2++) {
+        if (info[PEPE1].userId === info[PEPE2].userId) {
+          if (info[PEPE2].completed === true) {
+            mapi[useMe] = mapi[useMe] + 1;
+          }
         }
       }
     }
   }
+
   console.log(mapi);
 });
